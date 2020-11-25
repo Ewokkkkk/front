664f415404f4fd9e0482410c4e87a937bda64d17
Ewokkkkk <ewok.mfk@gmail.com>
2020-11-25 22:18:30 +0900

    npm, axiosの追加

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 "use strict";
+const axios = require("axios");
+// const jsdom = require("jsdom");
 
-let getData = async (url) => {
-  const response = await fetch(url);
-  const jsonData = await response.json();
+const getData = async (url) => {
+  // const response = await fetch(url);
+  const res = await axios.get(url);
+  const data = res.data;
+  console.log(data);
+  // for (let i in data.result) {
+  //   const ul = document.getElementById('ul');
+  //   const li = document.createElement('li');
+  //   const img = document.createElement('img');
+  //   const title = document.createElement('h3');
+  //   img.src = data.result[i].foodImageUrl;
+  //   title.textContent = data.result[i].recipeTitle;
+  //   li.appendChild(img);
+  //   li.appendChild(title);
+  //   ul.appendChild(li);
 
-  for (let i in jsonData.result) {
-    const ul = document.getElementById('ul');
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    const title = document.createElement('h3');
-    img.src = jsonData.result[i].foodImageUrl;
-    title.textContent = jsonData.result[i].recipeTitle;
-    li.appendChild(img);
-    li.appendChild(title);
-    ul.appendChild(li);
-
-    // liをクリックしたら非表示に
-    li.addEventListener("click", () => {
-      li.style.visibility = "hidden";
-    });
-  };
+  //   // liをクリックしたら非表示に
+  //   li.addEventListener("click", () => {
+  //     li.style.visibility = "hidden";
+  //   });
+  // };
 };
-
-const baseURL = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=1086382364385531386&categoryId=";
-const url2 = `${baseURL}30`;
+const baseURL =
+  "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=1086382364385531386&categoryId=";
+// const url2 = `${baseURL}30`;
 const url1 = `${baseURL}32`;
 
 getData(url1);
-getData(url2);
+// getData(url2);
